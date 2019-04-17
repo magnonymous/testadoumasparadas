cb233371c97459230ea3000c01ee28c5148616f2
magnonymous <49704208+magnonymous@users.noreply.github.com>
2019-04-17 00:06:39 -0300

    Create Vendedor.java

---
+++ Vendedor.java
@@ -0,0 +1,50 @@
+package RegistraVenda;
+
+public class Vendedor {
+
+    private String nome;
+    private int codigo;
+    private float comissao;
+    private String endereco;
+
+    //Construtor
+    //Recebendo o nome, o endereco e o codigo.
+    public Vendedor(String nome, int codigo, String endereco) {
+        super();
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(float comissao) {
+        this.comissao = comissao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
